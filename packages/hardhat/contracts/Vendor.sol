0de72a6e86aa3e5a217747eabf6154c4bcc48403
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-04-02 17:57:16 +0400

    challenge-2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -6,18 +6,39 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  
   YourToken public yourToken;
+  uint256 public constant tokensPerEth = 100;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
+  constructor(address tokenAddress) { 
+    yourToken = YourToken(tokenAddress); 
+  } 
+ 
+  // ToDo: create a payable buyTokens() function: 
+  function buyTokens() public payable { 
+ 
+    uint amountOfTokens = tokensPerEth * msg.value;  
+    yourToken.transfer(msg.sender, amountOfTokens); 
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens); 
+  } 
+ 
+  // ToDo: create a withdraw() function that lets the owner withdraw ETH 
+  function withdraw() public onlyOwner { 
+ 
+    uint totalAmount = address(this).balance; 
+    address owner=msg.sender; 
+    require(totalAmount > 0, "Not enough Eth available"); 
+    (bool success,) = owner.call{value: totalAmount}(""); 
+    require(success, "Failed to withdraw Eth");
   }
 
-  // ToDo: create a payable buyTokens() function:
-
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
-  // ToDo: create a sellTokens() function:
-
-}
+  // ToDo: create a sellTokens() function: 
+  function sellTokens(uint256 amount) public {
+    uint256 saleAmount = amount/tokensPerEth;
+    yourToken.transferFrom(msg.sender, address(this), amount);
+    (bool sent, bytes memory data) = msg.sender.call{value: saleAmount}("");
+    emit SellTokens(msg.sender, amount, saleAmount);
+  }
+} 
--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -25,7 +25,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "rinkeby";
 
 const mainnetGwei = 115;
 
@@ -60,7 +60,7 @@ module.exports = {
       */
     },
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://rinkeby.infura.io/v3/b95d442af63a4ab7814053fbe0bb07b5", // <---- YOUR INFURA ID! (or it won't work)
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
